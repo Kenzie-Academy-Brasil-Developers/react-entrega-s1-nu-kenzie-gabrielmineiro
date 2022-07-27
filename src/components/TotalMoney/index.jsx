b5f6edd6f6be2707376aa16d45a30363c5be58ab
gabriel <gabrielmineironogueira@gmail.com>
2022-07-27 16:07:56 -0300

    alterando Valor total

--- src/components/TotalMoney/index.jsx
+++ src/components/TotalMoney/index.jsx
@@ -1,20 +1,41 @@
 function TotalMoney({ lista }) {
-  let array = [];
+  let arrayPositivo = [];
+  let arrayNegativo = [];
   for (let i = 0; i < lista.length; i++) {
-    array.push(lista[i].valor);
+    lista[i].tipo === "Entrada"
+      ? arrayPositivo.push(lista[i].valor)
+      : arrayNegativo.push(lista[i].valor);
   }
 
-  let numero = [];
-  for (let i = 0; i < array.length; i++) {
-    let transforma = parseInt(array[i]);
-    numero.push(transforma);
+  let numeroPositivo = [];
+  for (let i = 0; i < arrayPositivo.length; i++) {
+    let transforma = parseInt(arrayPositivo[i]);
+    numeroPositivo.push(transforma);
   }
+
+  let numeroNegativo = [];
+  for (let i = 0; i < arrayNegativo.length; i++) {
+    let transforma = parseInt(arrayNegativo[i]);
+    numeroNegativo.push(transforma);
+  }
+
+  let soma = numeroPositivo.reduce(
+    (oldValue, currentValue) => oldValue + currentValue,
+    0
+  );
+  let diminui = numeroNegativo.reduce(
+    (oldValue, currentValue) => oldValue + currentValue,
+    0
+  );
+  let resultado = soma - diminui;
+
+  console.log(lista);
   return (
     <div className="totalMoney">
       <p className="valorTotalMoney"> Valor Total:</p>
       <p className="precoTotalMoney">
         R$
-        {numero.reduce((oldValue, currentValue) => oldValue + currentValue, 0)}
+        {resultado}
       </p>
     </div>
   );
